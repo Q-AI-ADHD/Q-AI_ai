2a3ed505c78c6ccd342d69ecda7cc4379c3e4575
yeeeengyu <chldlsrb08@naver.com>
2026-01-18 13:43:31 +0900

    feature: pdf스키마 추가

--- schemas/questSchema.py
+++ schemas/questSchema.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
+from fastapi import UploadFile, File
 
 class questionRequest(BaseModel):
     subject: str
     level: str
     subjectdetail: str
+    isPdf: Optional[UploadFile] = File(None)
     
 class questionResponse(BaseModel):
     question: str
